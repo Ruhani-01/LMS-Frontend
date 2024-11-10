e9afa537db0c9cf1bd5b0d078e45a7212ad4f9ae
Ruhani-01 <ruhanimaluja20@gmail.com>
2024-11-10 22:33:27 +0530

    Axios

--- src/components/AllCourses/courses/Courses.jsx
+++ src/components/AllCourses/courses/Courses.jsx
@@ -12,7 +12,7 @@ const App = () => {
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const coursesResponse = await fetch("https://learnlynxbackend.onrender.com/api/admin/courses");
+        const coursesResponse = await axios.get("https://learnlynxbackend.onrender.com/api/admin/courses");
         if (!coursesResponse.ok) {
           throw new Error("Failed to fetch courses");
         }
