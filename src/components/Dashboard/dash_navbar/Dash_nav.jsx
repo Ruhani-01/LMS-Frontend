a0873a5a5b242484c0c429ccb911f9bc7351337a
Manan Bhasin <mananbhasin5657@gmail.com>
2024-12-08 19:35:07 +0530

    Frontend manan final commit

--- src/components/Dashboard/dash_navbar/Dash_nav.jsx
+++ src/components/Dashboard/dash_navbar/Dash_nav.jsx
@@ -2,22 +2,18 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Dash_nav.css";
 import Logo from "../assets/logo.png";
-import Dashboard from "../assets/dashboard.png";
-import Mycourses from "../assets/mycourses.png";
-import Students from "../assets/students.png";
-import Schedule from "../assets/Schedule.png";
-import Liveclass from "../assets/liveclass.png";
-import Chat from "../assets/chat.png";
-import Myaccount from "../assets/myaccount.png";
-import Notify from "../assets/notify.png";
-import Logout from "../assets/logout.png";
-import Notify2 from "../assets/notify2.png";
-import Dashboard2 from "../assets/Dashboard2.png";
-import Mycourses2 from "../assets/mycourses2.png";
-import Students2 from "../assets/students2.png";
-import Schedule2 from "../assets/schedule2.png";
-import Liveclass2 from "../assets/liveclass2.png";
-import Chat2 from "../assets/chat2.png";
+
+// React Icons
+import {
+  FaThLarge,
+  FaThList,
+  FaUserGraduate,
+  FaCalendarAlt,
+  FaVideo,
+  FaCommentAlt,
+  FaBell,
+  FaSignOutAlt,
+} from "react-icons/fa";
 
 function Dash_nav() {
   const id = localStorage.getItem("id");
@@ -37,16 +33,10 @@ function Dash_nav() {
               : "dashboard-navbar-navlink"
           }
         >
-          {({ isActive }) => (
-            <p className="dashboard-navbar-link">
-              <img
-                src={isActive ? Dashboard2 : Dashboard}
-                alt="dashboard"
-                className="dashboard"
-              />
-              Dashboard
-            </p>
-          )}
+          <p className="dashboard-navbar-link">
+            <FaThLarge className="dashboard-icon" />
+            Dashboard
+          </p>
         </NavLink>
         <NavLink
           to={`/admin/${id}/mycourses`}
@@ -56,12 +46,10 @@ function Dash_nav() {
               : "dashboard-navbar-navlink"
           }
         >
-          {({ isActive }) => (
-            <p className="dashboard-navbar-link">
-              <img src={isActive ? Mycourses2 : Mycourses} alt="my courses" />
-              My Courses
-            </p>
-          )}
+          <p className="dashboard-navbar-link">
+            <FaThList className="dashboard-icon" />
+            My Courses
+          </p>
         </NavLink>
         <NavLink
           to={`/admin/${id}/students`}
@@ -71,12 +59,10 @@ function Dash_nav() {
               : "dashboard-navbar-navlink"
           }
         >
-          {({ isActive }) => (
-            <p className="dashboard-navbar-link">
-              <img src={isActive ? Students2 : Students} alt="students" />
-              Students
-            </p>
-          )}
+          <p className="dashboard-navbar-link">
+            <FaUserGraduate className="dashboard-icon" />
+            Students
+          </p>
         </NavLink>
         <NavLink
           to={`/admin/${id}/schedule`}
@@ -86,12 +72,10 @@ function Dash_nav() {
               : "dashboard-navbar-navlink"
           }
         >
-          {({ isActive }) => (
-            <p className="dashboard-navbar-link">
-              <img src={isActive ? Schedule2 : Schedule} alt="schedule" />
-              Schedule
-            </p>
-          )}
+          <p className="dashboard-navbar-link">
+            <FaCalendarAlt className="dashboard-icon" />
+            Schedule
+          </p>
         </NavLink>
         <NavLink
           to={`/admin/${id}/liveclass`}
@@ -101,12 +85,10 @@ function Dash_nav() {
               : "dashboard-navbar-navlink"
           }
         >
-          {({ isActive }) => (
-            <p className="dashboard-navbar-link">
-              <img src={isActive ? Liveclass2 : Liveclass} alt="live class" />
-              Live Class
-            </p>
-          )}
+          <p className="dashboard-navbar-link">
+            <FaVideo className="dashboard-icon" />
+            Live Class
+          </p>
         </NavLink>
         <NavLink
           to={`/admin/${id}/chat`}
@@ -116,12 +98,10 @@ function Dash_nav() {
               : "dashboard-navbar-navlink"
           }
         >
-          {({ isActive }) => (
-            <p className="dashboard-navbar-link">
-              <img src={isActive ? Chat2 : Chat} alt="chat" />
-              Chat
-            </p>
-          )}
+          <p className="dashboard-navbar-link">
+            <FaCommentAlt className="dashboard-icon" />
+            Chat
+          </p>
         </NavLink>
         <NavLink
           to={`/admin/${id}/notify`}
@@ -132,14 +112,15 @@ function Dash_nav() {
           }
         >
           <p className="dashboard-navbar-link">
-            <img src={Notify} alt="my account" />
+            <FaBell className="dashboard-icon" />
             Notify
           </p>
         </NavLink>
       </div>
       <div>
         <button className="navbar-button">
-          <img src={Logout} alt="logout" /> <p>Log Out</p>
+          <FaSignOutAlt className="dashboard-icon" />
+          <p>Log Out</p>
         </button>
       </div>
     </div>
@@ -147,3 +128,153 @@ function Dash_nav() {
 }
 
 export default Dash_nav;
+
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import "./Dash_nav.css";
+// import Logo from "../assets/logo.png";
+// import Dashboard from "../assets/dashboard.png";
+// import Mycourses from "../assets/mycourses.png";
+// import Students from "../assets/students.png";
+// import Schedule from "../assets/Schedule.png";
+// import Liveclass from "../assets/liveclass.png";
+// import Chat from "../assets/chat.png";
+// import Myaccount from "../assets/myaccount.png";
+// import Notify from "../assets/notify.png";
+// import Logout from "../assets/logout.png";
+// import Notify2 from "../assets/notify2.png";
+// import Dashboard2 from "../assets/Dashboard2.png";
+// import Mycourses2 from "../assets/mycourses2.png";
+// import Students2 from "../assets/students2.png";
+// import Schedule2 from "../assets/schedule2.png";
+// import Liveclass2 from "../assets/liveclass2.png";
+// import Chat2 from "../assets/chat2.png";
+
+// function Dash_nav() {
+//   const id = localStorage.getItem("id");
+
+//   return (
+//     <div className="dashboard-navbar2">
+//       <div className="dashboard-navbar-logo">
+//         <img src={Logo} alt="logo" />
+//         <h1>LearnLynx</h1>
+//       </div>
+//       <div className="dashboard-navbar-links">
+//         <NavLink
+//           to={`/admin/${id}/dashboard`}
+//           className={({ isActive }) =>
+//             isActive
+//               ? "dashboard-navbar-navlink active"
+//               : "dashboard-navbar-navlink"
+//           }
+//         >
+//           {({ isActive }) => (
+//             <p className="dashboard-navbar-link">
+//               <img
+//                 src={isActive ? Dashboard2 : Dashboard}
+//                 alt="dashboard"
+//                 className="dashboard"
+//               />
+//               Dashboard
+//             </p>
+//           )}
+//         </NavLink>
+//         <NavLink
+//           to={`/admin/${id}/mycourses`}
+//           className={({ isActive }) =>
+//             isActive
+//               ? "dashboard-navbar-navlink active"
+//               : "dashboard-navbar-navlink"
+//           }
+//         >
+//           {({ isActive }) => (
+//             <p className="dashboard-navbar-link">
+//               <img src={isActive ? Mycourses2 : Mycourses} alt="my courses" />
+//               My Courses
+//             </p>
+//           )}
+//         </NavLink>
+//         <NavLink
+//           to={`/admin/${id}/students`}
+//           className={({ isActive }) =>
+//             isActive
+//               ? "dashboard-navbar-navlink active"
+//               : "dashboard-navbar-navlink"
+//           }
+//         >
+//           {({ isActive }) => (
+//             <p className="dashboard-navbar-link">
+//               <img src={isActive ? Students2 : Students} alt="students" />
+//               Students
+//             </p>
+//           )}
+//         </NavLink>
+//         <NavLink
+//           to={`/admin/${id}/schedule`}
+//           className={({ isActive }) =>
+//             isActive
+//               ? "dashboard-navbar-navlink active"
+//               : "dashboard-navbar-navlink"
+//           }
+//         >
+//           {({ isActive }) => (
+//             <p className="dashboard-navbar-link">
+//               <img src={isActive ? Schedule2 : Schedule} alt="schedule" />
+//               Schedule
+//             </p>
+//           )}
+//         </NavLink>
+//         <NavLink
+//           to={`/admin/${id}/liveclass`}
+//           className={({ isActive }) =>
+//             isActive
+//               ? "dashboard-navbar-navlink active"
+//               : "dashboard-navbar-navlink"
+//           }
+//         >
+//           {({ isActive }) => (
+//             <p className="dashboard-navbar-link">
+//               <img src={isActive ? Liveclass2 : Liveclass} alt="live class" />
+//               Live Class
+//             </p>
+//           )}
+//         </NavLink>
+//         <NavLink
+//           to={`/admin/${id}/chat`}
+//           className={({ isActive }) =>
+//             isActive
+//               ? "dashboard-navbar-navlink active"
+//               : "dashboard-navbar-navlink"
+//           }
+//         >
+//           {({ isActive }) => (
+//             <p className="dashboard-navbar-link">
+//               <img src={isActive ? Chat2 : Chat} alt="chat" />
+//               Chat
+//             </p>
+//           )}
+//         </NavLink>
+//         <NavLink
+//           to={`/admin/${id}/notify`}
+//           className={({ isActive }) =>
+//             isActive
+//               ? "dashboard-navbar-navlink active"
+//               : "dashboard-navbar-navlink"
+//           }
+//         >
+//           <p className="dashboard-navbar-link">
+//             <img src={Notify} alt="my account" />
+//             Notify
+//           </p>
+//         </NavLink>
+//       </div>
+//       <div>
+//         <button className="navbar-button">
+//           <img src={Logout} alt="logout" /> <p>Log Out</p>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Dash_nav;
